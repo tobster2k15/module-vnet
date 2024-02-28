9e6244a2a3857832d30415fb7095ef5ebc0c5ca7
tobster2k15 <150067621+tobster2k15@users.noreply.github.com>
2024-02-28 11:19:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_subnet" "subnet_count" {
   name                                           = var.subnet_names[count.index]
   resource_group_name                            = var.resource_group_name
   virtual_network_name                           = azurerm_virtual_network.vnet.name
-  enforce_private_link_endpoint_network_policies = lookup(var.subnet_enforce_private_link_endpoint_network_policies, var.subnet_names[count.index], false)
+  private_endpoint_network_policies_enabled = lookup(var.subnet_private_endpoint_network_policies_enabled, var.subnet_names[count.index], false)
   private_link_service_network_policies_enabled = lookup(var.subnet_enforce_private_link_service_network_policies, var.subnet_names[count.index], false)
   service_endpoints                              = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], null)
 
@@ -53,7 +53,7 @@ resource "azurerm_subnet" "subnet_for_each" {
   name                                           = each.value
   resource_group_name                            = var.resource_group_name
   virtual_network_name                           = azurerm_virtual_network.vnet.name
-  enforce_private_link_endpoint_network_policies = lookup(var.subnet_enforce_private_link_endpoint_network_policies, each.value, false)
+  private_endpoint_network_policies_enabled = lookup(var.subnet_private_endpoint_network_policies_enabled, each.value, false)
   private_link_service_network_policies_enabled = lookup(var.subnet_enforce_private_link_service_network_policies, each.value, false)
   service_endpoints                              = lookup(var.subnet_service_endpoints, each.value, null)
 

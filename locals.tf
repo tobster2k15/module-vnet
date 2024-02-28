6ffcbc367b77b53f86bee9863c466cd329f81143
tbraunzech <tobias.braun@wf-ib.de>
2024-02-28 10:40:30 +0100

    changed locals.tf

--- locals.tf
+++ locals.tf
@@ -1,20 +1,3 @@
-locals{
-    common_tags = {
-    OPE                 = "${var.OPE}"
-    "Cost Center"       = "${var.CostCenter}"
-    "Responsible Team"  = "${var.ResponsibleTeam}"
-    "Deployed by"       = "${var.deployment}"
-    "Environment"       = "${var.tag_env_prd}"
-    "Usecase"           = "${var.usecase_for_desc}"
-}
-
-#Dev Tags
-common_tags_dev = {
-    OPE                 = "${var.OPE}"
-    "Cost Center"       = "${var.CostCenter}"
-    "Responsible Team"  = "${var.ResponsibleTeam}"
-    "Deployed by"       = "${var.deployment}"
-    "Environment"       = "${var.tag_env_dev}"
-    "Usecase"           = "${var.usecase_for_desc}"
-}
+locals {
+  subnet_names_prefixes = zipmap(var.subnet_names, var.subnet_prefixes)
 }

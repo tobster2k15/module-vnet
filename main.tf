fe67f820584f30ebaf10bec35b654d189325e559
tbraunzech <tobias.braun@wf-ib.de>
2024-02-29 08:29:49 +0100

    added public ip

--- main.tf
+++ main.tf
@@ -92,3 +92,12 @@ resource "azurerm_subnet_route_table_association" "vnet" {
   route_table_id = each.value
   subnet_id      = local.azurerm_subnets_name_id_map[each.key]
 }
+
+resource "azurerm_public_ip" "pip" {
+  for_each = var.pip_ids
+
+  location                = var.vnet_location
+  name                    = each.key
+  resource_group_name     = var.resource_group_name
+  allocation_method       = "Static"
+}
